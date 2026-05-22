cb0ba42824a8e2879ae3a26b94c5d330b35f769f
RYANMEGA63 <ryanaitbessai@gmail.com>
2026-05-22 23:27:45 +0200

    fifth commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 ﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     tsconfigPaths(),
     tanstackStart({
       server: { entry: "server" },
     }),
+    react(),
   ],
   optimizeDeps: {
     exclude: ["animejs"],
